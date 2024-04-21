2744da392e7b5680a63538dbddbdf82688ff0e9d
Dzyanino <111046359+Dzyanino@users.noreply.github.com>
2024-04-21 23:53:06 +0300

    Fix : Backend server (now using self-hosted supabase)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  devtools: { enabled: false },
+
   app: {
     head: {
       charset: "utf-8",
@@ -10,8 +12,15 @@ export default defineNuxtConfig({
     pageTransition: { name: 'slide', mode: 'out-in' }
   },
 
-  devtools: { enabled: false },
-  modules: ["vuetify-nuxt-module"],//"@nuxtjs/supabase", 
+  modules: ["@nuxtjs/supabase", "vuetify-nuxt-module"],
+
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    serviceKey: process.env.SUPABASE_SERVICE_KEY,
+    redirect: false,
+  },
+
   vuetify: {
     vuetifyOptions: {
       directives: true,
