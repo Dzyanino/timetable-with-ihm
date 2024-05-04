0c0bd5de0eb24b9b2ebe49706bd0275fca9dee61
Dzyanino <111046359+Dzyanino@users.noreply.github.com>
2024-05-04 19:12:58 +0300

    Add : /classe server route

---
+++ server/api/classe.post.ts
@@ -0,0 +1,13 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    const supabase = await serverSupabaseClient(event);
+    const body = await readBody(event);
+
+    const { data } = await supabase
+        .from("classe_view")
+        .select("*")
+        .eq("CodeNiveau", body.niveau);
+
+    return { classe: data };
+});
---
+++ server/api/edt.post.ts
@@ -0,0 +1,15 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    const supabase = await serverSupabaseClient(event);
+    const body = await readBody(event);
+
+    const { data } = await supabase
+        .from("edt_view")
+        .select("*")
+        .eq("CodeNiveau", body.niveau)
+        .gte("Date", body.debutSemaine)
+        .lte("Date", body.finSemaine);
+
+    return { edt: data };
+});
