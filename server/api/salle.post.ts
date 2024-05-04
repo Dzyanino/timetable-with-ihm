642a696f5cad486fdc7337dfc60be4d1341c6840
Dzyanino <111046359+Dzyanino@users.noreply.github.com>
2024-05-04 22:31:39 +0300

    Add : /salle server route

---
+++ server/api/salle.post.ts
@@ -0,0 +1,12 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    const supabase = await serverSupabaseClient(event);
+    const body = await readBody(event);
+
+    const { data } = await supabase
+        .from("Salle")
+        .select("*")
+    
+    return { salle: data };
+});
