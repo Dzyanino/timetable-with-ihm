b561f1424de9d41ac44f92c789b66625ca04fb64
Dzyanino <111046359+Dzyanino@users.noreply.github.com>
2024-05-29 16:39:48 +0300

    Change : Came back to supabase online with some updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  // ssr: false,
+    devtools: { enabled: false },
+    // ssr: false,
 
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
+    app: {
+        head: {
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+        },
+        layoutTransition: { name: "layout", mode: "out-in" },
+        pageTransition: { name: "slide", mode: "out-in" },
     },
-    layoutTransition: { name: 'layout', mode: 'out-in' },
-    pageTransition: { name: 'slide', mode: 'out-in' }
-  },
 
-  modules: ["@nuxtjs/supabase", "vuetify-nuxt-module"],
+    modules: ["@nuxtjs/supabase", "vuetify-nuxt-module"],
 
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    serviceKey: process.env.SUPABASE_SERVICE_KEY,
-    redirect: false,
-  },
+    supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY,
+        // serviceKey: process.env.SUPABASE_SERVICE_KEY,
+        redirect: false,
+    },
 
-  vuetify: {
-    vuetifyOptions: {
-      directives: true,
-      locale: {
-        locale: "fr",
-      },
-      // date: {
-      //   locale: {
-      //     fr: "fr-FR"
-      //   }
-      // },
-      icons: {
-        defaultSet: "mdi",
-      }
-    }
-  }
-})
+    vuetify: {
+        vuetifyOptions: {
+            directives: true,
+            locale: {
+                locale: "fr",
+            },
+            // date: {
+            //   locale: {
+            //     fr: "fr-FR"
+            //   }
+            // },
+            icons: {
+                defaultSet: "mdi",
+            },
+        },
+    },
+});
